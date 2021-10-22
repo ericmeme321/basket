cf0695b1386ee156647bbd0831220b17724057a9
Eric <ericmeme321@gmail.com>
2021-10-22 17:09:53 +0800

    2021/10/22

--- push.py
+++ push.py
@@ -1,6 +1,8 @@
 import os
 import datetime
 
-today = datetime.datetime.today().strftime("%Y%m%d")
+today = datetime.datetime.today().strftime("%Y/%m/%d")
+
 os.system("git add .")
 os.system(f"git commit -m {today}")
+os.system("git push origin master")
